f20f3b675a5c66c5cf5d563d4c32eab5d130ace3
paura432 <pauramosimo@gmail.com>
2025-09-12 13:13:58 +0200

    changed next.conf

--- front/next.config.ts
+++ front/next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  basePath: '/login_cert',
+  trailingSlash: true,     // <-- CLAVE: evita el bucle 301↔308
+  reactStrictMode: true,
 };
+
+module.exports = nextConfig;
 
 export default nextConfig;
